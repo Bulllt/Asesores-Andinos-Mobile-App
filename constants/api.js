05c616c8d96f2f1b8023ebe889116ff51e6a72dc
Bull <papipiro2003@gmail.com>
2025-04-20 19:29:54 -0400

    added URL's to the api constant

---
+++ constants/api.js
@@ -0,0 +1,9 @@
+const auth = "https://api.csep.dev/api/auth/";
+const adminCategory = "https://api.csep.dev/api/admin/category/";
+const adminInventory = "https://api.csep.dev/api/admin/inventory/";
+const adminInvInbound = "https://api.csep.dev/api/admin/inventoryOrderInbound/";
+const adminInvOutbound =
+  "https://api.csep.dev/api/admin/inventoryOrderOutbound/";
+const adminItem = "https://api.csep.dev/api/admin/item/";
+const adminOrderInbound = "https://api.csep.dev/api/admin/orderInbound/";
+const adminOrderOutbound = "https://api.csep.dev/api/admin/orderOutbound/";
